166477f4ca3d8f5347f7f49a6071f948ec9b2b12
Jeffrey Blagdon <jeffblagdon@gmail.com>
2016-06-17 12:28:11 -0400

    Add PhotoDisplayViewController

---
+++ CameraCollector/PhotoDisplayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoDisplayViewController.swift
+//  CameraCollector
+//
+//  Created by Jeffrey Blagdon on 6/17/16.
+//  Copyright © 2016 Minibar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoDisplayViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func buttonTapped(sender: AnyObject) {
+    }
+}
