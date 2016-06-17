e2790cf73bc037f65747cf6a1ab6cb90e072540d
Jeffrey Blagdon <jeffblagdon@gmail.com>
2016-06-17 13:32:05 -0400

    Adding images

--- CameraCollector/CollectionViewController.swift
+++ CameraCollector/CollectionViewController.swift
@@ -16,7 +16,7 @@ class ImageCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
 }
 
-class CollectionViewController: UICollectionViewController {        // Our type's name is CollectionViewController
+class CollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {        // Our type's name is CollectionViewController
     
     var photos: [UIImage] = []      // An array to hold our photos. Var means we can change its contents.
     
@@ -33,11 +33,31 @@ class CollectionViewController: UICollectionViewController {        // Our type'
             }
         } else {
             if let imageCell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageCell", forIndexPath: indexPath) as? ImageCell {      // Is the cell that you have for the "ImageCell" identifier actually an ImageCell?
+                let photo = photos[indexPath.item - 1]
+                imageCell.imageView.image = photo
                 return imageCell                // Return it
             } else {
                 return UICollectionViewCell()   // Whatever
             }
         }
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let item = indexPath.section
+        if item == 0 {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            presentViewController(pickerController, animated: true, completion: nil)
+        } else {
+            
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        photos.append(image)
+        let newPath = NSIndexPath(forItem: photos.count, inSection: 0)
+        collectionView?.insertItemsAtIndexPaths([newPath])
     }
 }
 
